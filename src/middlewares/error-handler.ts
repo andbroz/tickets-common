9e5cef5319190139198ce4be80ec62a42ce62873
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2022-06-12 09:55:19 +0200

    update err handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -6,6 +6,8 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
+
   res.status(400).send({
     errors: [
       {
